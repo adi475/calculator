5b2b2202dadfc2422186630568ce9adae136019a
adi475 <chaudhary.aditya475@gmail.com>
2024-02-18 12:06:17 +0530

    added github and linkedin links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
       <div className='footer'>
         <ul>
             <li>contact us :
-               <br /> <i className="fa fa-linkedin-square">&nbsp;<a href="/">linkedin</a></i>
+               <br /> <i className="fa fa-linkedin-square">&nbsp;<a href="https://www.linkedin.com/in/aditya-chaudhary-6509a845">linkedin</a></i>
                <br /> <i className="fa fa-youtube-square">&nbsp;<a href="/">youtube</a></i>
-               <br /> <i className="fa fa-github-square">&nbsp;<a href="/">github</a></i>
+               <br /> <i className="fa fa-github-square">&nbsp;<a href="https://github.com/adi475">github</a></i>
                </li>
             <li>created by :
               <br /><pre>Aditya chaudhary</pre>
